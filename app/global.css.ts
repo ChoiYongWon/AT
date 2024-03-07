40d87699d124ef8972851d76213cf16c19db7d37
ChoiYongWon <yongwon0824@naver.com>
2024-03-07 17:07:57 +0900

    init: 폰트 및 vanilla-extract, storybook 설정

---
+++ app/global.css.ts
@@ -0,0 +1,43 @@
+import {
+  fontFace,
+  style,
+  globalFontFace,
+  globalStyle,
+} from "@vanilla-extract/css";
+
+const nanumSquareNeo = "NanumSquareNeo";
+const poppins = "Poppins";
+
+globalFontFace(nanumSquareNeo, {
+  src: "url(/fonts/NanumSquareNeo-Variable.ttf), format('ttf')",
+  fontWeight: "light",
+});
+
+globalFontFace(nanumSquareNeo, {
+  src: "url(/fonts/NanumSquareNeo-Variable.ttf), format('ttf')",
+  fontWeight: "normal",
+});
+
+globalFontFace(nanumSquareNeo, {
+  src: "url(/fonts/NanumSquareNeo-Variable.ttf), format('ttf')",
+  fontWeight: "bold",
+});
+
+globalFontFace(poppins, {
+  src: "url(/fonts/Poppins-Light.ttf), format('ttf')",
+  fontWeight: "light",
+});
+
+globalFontFace(poppins, {
+  src: "url(/fonts/Poppins-Regular.ttf), format('ttf')",
+  fontWeight: "normal",
+});
+
+globalFontFace(poppins, {
+  src: "url(/fonts/Poppins-Bold.ttf), format('ttf')",
+  fontWeight: "bold",
+});
+
+globalStyle("html, body", {
+  fontFamily: `${nanumSquareNeo}, ${poppins}`,
+});
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withVanillaExtract(nextConfig);
