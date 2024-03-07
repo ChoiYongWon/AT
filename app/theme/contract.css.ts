2da221bdd5bfc9fa275a90f6a2dde30b02976b8e
ChoiYongWon <yongwon0824@naver.com>
2024-03-08 04:22:31 +0900

    style: 테마 설정

---
+++ app/theme/contract.css.ts
@@ -0,0 +1,39 @@
+import { createGlobalThemeContract } from "@vanilla-extract/css";
+
+export const vars = createGlobalThemeContract({
+  color: {
+    primary: "primary",
+    secondary: "secondary",
+    fontBlack: "font-black",
+    strokeBlack: "stroke-black",
+    white: "white",
+    background: "background",
+    point_1: "point-1",
+    point_2: "point-2",
+    point_3: "point-3",
+  },
+
+  font: {
+    eng: "eng",
+    kor: "kor",
+  },
+
+  text: {
+    mobile: {
+      title_1: "title-1",
+      title_2: "title-2",
+      subTitle_2: "subtitle-2",
+      button_1: "button-1",
+      button_2: "button-2",
+      content: "content",
+    },
+    desktop: {
+      title_1: "title-1",
+      title_2: "title-2",
+      subTitle_2: "subtitle-2",
+      button_1: "button-1",
+      button_2: "button-2",
+      content: "content",
+    },
+  },
+});
---
+++ app/theme/theme.css.ts
@@ -0,0 +1,40 @@
+import { createGlobalTheme } from "@vanilla-extract/css";
+import { vars } from "./contract.css";
+
+createGlobalTheme(":root", vars, {
+  color: {
+    primary: "#EF6F12",
+    secondary: "#FDAE74",
+    fontBlack: "#222222",
+    strokeBlack: "#202020",
+    white: "#FFFFFF",
+    background: "#EEEEEE",
+    point_1: "#04C75B",
+    point_2: "#FAE100",
+    point_3: "#4285F4",
+  },
+
+  font: {
+    eng: "Poppins",
+    kor: "NanumSquareNeo",
+  },
+
+  text: {
+    mobile: {
+      title_1: "24px",
+      title_2: "20px",
+      subTitle_2: "18px",
+      button_1: "18px",
+      button_2: "12px",
+      content: "16px",
+    },
+    desktop: {
+      title_1: "38px",
+      title_2: "30px",
+      subTitle_2: "28px",
+      button_1: "28px",
+      button_2: "18px",
+      content: "24px",
+    },
+  },
+});
