91529f80f8a407c423b91403ce1b8d1e032b4f62
ChoiYongWon <yongwon0824@naver.com>
2024-03-20 00:50:46 +0900

    bug: getToken salt 인식 에러

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,7 @@ export default auth(async (req) => {
     secret: process.env.AUTH_SECRET as any,
     salt: process.env.AUTH_SALT as any,
   });
+  console.log(process.env.AUTH_SALT, session);
 
   if (!session) return NextResponse.redirect(new URL("/login", req.url));
 });
