631befd9191299fd7549f81d29411f417f6f072a
ChoiYongWon <yongwon0824@naver.com>
2024-03-20 00:33:29 +0900

    bug: prisma 버그 수정 supabase pgbouncer=true Issue

--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,12 @@ import { auth } from "./auth";
 import { getToken } from "next-auth/jwt";
 
 export default auth(async (req) => {
-  // console.log("DECODE", decoded);
   const session = await getToken({
     req,
     secret: process.env.AUTH_SECRET as any,
     salt: process.env.AUTH_SALT as any,
   });
-  // console.log(session);
   if (!session) return NextResponse.redirect(new URL("/login", req.url));
-  // if (!req.auth?.user) return NextResponse.redirect(new URL("/login", req.url));
 });
 
 // Optionally, don't invoke Middleware on some paths
