84f398a5596501f22147b9a368d6b3f62aced248
ChoiYongWon <yongwon0824@naver.com>
2024-03-27 02:33:42 +0900

    bug: #5 캐시 정리

--- app/Provider/AnimateProvider/index.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { motion, AnimatePresence } from "framer-motion";
-
-const AnimateProvider = ({ children }: { children: any }) => {
-  return <AnimatePresence>{children}</AnimatePresence>;
-};
-
-export default AnimateProvider;
--- app/Provider/AuthContext/index.tsx
+++
@@ -1,7 +0,0 @@
-import { SessionProvider } from "next-auth/react";
-
-const AuthContext = ({ children }: { children: any }) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
-
-export default AuthContext;
