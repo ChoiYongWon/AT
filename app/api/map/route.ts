6147fe709271750ccf10aa8803f6c6e3da52e90e
ChoiYongWon <yongwon0824@naver.com>
2024-04-13 05:52:34 +0900

    dummy: #5 console.log 삭제 2

--- app/api/map/route.ts
+++ app/api/map/route.ts
@@ -57,8 +57,6 @@ export async function POST(request: NextRequest) {
 export async function GET(request: NextRequest) {
   try {
     const session = await useAuth();
-    console.log("들어옴")
-
     const searchParams = request.nextUrl.searchParams
     const userId = searchParams.get('userId')
 
