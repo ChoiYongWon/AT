27f18e2393d3cd8f184662c72431bdb0a043ea5b
ChoiYongWon <yongwon0824@naver.com>
2024-04-30 10:41:19 +0900

    feat: #8 캐시 1년으로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -54,7 +54,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=86400, immutable',
+            value: 'public, max-age=31536000, immutable',
           },
         ],
 
