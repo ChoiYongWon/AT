2c4bb2886a5bdcaaa568cd174c8347aa22518912
ChoiYongWon <yongwon0824@naver.com>
2024-05-13 18:50:15 +0900

    feat: #11 profile loading 추가

--- app/profile/style.css.ts
+++ app/profile/style.css.ts
@@ -63,3 +63,33 @@ export const ProfileLayoutStyle = style({
     fontSize: "18px",
   });
   
+
+
+export const LoadingLayoutStyle = style({
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#F7F7F7",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+})
+
+export const LoadingWrapperStyle = style({
+    display: "flex",
+    flexDirection: "column",
+    gap: "8px",
+    alignItems: "center"
+})
+
+export const LoadingTextStyle = style({
+    textAlign: "center",
+    fontSize: "14px"
+})
+
+export const LoadingImageStyle = style({
+    width: "80px",
+    height: "80px",
+})
