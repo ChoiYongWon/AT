bea59630df7b6c6de6fe6b0859fa40d507b1b348
ChoiYongWon <yongwon0824@naver.com>
2024-05-20 19:01:39 +0900

    bug: #21 Add 에도 적용

--- app/[[...map]]/component/AddButton/index.tsx
+++ app/[[...map]]/component/AddButton/index.tsx
@@ -3,19 +3,27 @@
 import Image from "next/image";
 import AddImage from "../../../../public/images/AddButton.svg";
 import { ButtonImageStyle, ButtonLinkStyle, ButtonStyle } from "./style.css";
-import Link from "next/link";
+// import Link from "next/link";
 import { motion } from "framer-motion";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 
 
 const AddButton = () => {
 
+  const router = useRouter()
+
+  useEffect(()=>{
+    router.prefetch("/add");
+  }, [router])
+
   return (
-      <Link className={ButtonStyle} href={"/add"} prefetch={true}>
+      <button className={ButtonStyle} onClick={()=>router.push("/add")}>
         <motion.div className={ButtonLinkStyle} {...{whileTap: {scale: 0.9, transition: { duration: 0.08 }}}}>
           <Image className={ButtonImageStyle} src={AddImage} alt="+" priority/>
         </motion.div>
-      </Link>
+      </button>
    
     
   );
