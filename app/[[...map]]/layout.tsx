5157644b47d25a2b6933fe54f451246628a47de0
ChoiYongWon <yongwon0824@naver.com>
2024-05-21 03:04:36 +0900

    feat: #23 at_id에 지역명이 들어가있을때 redirect

--- app/[[...map]]/layout.tsx
+++ app/[[...map]]/layout.tsx
@@ -2,6 +2,12 @@ import { auth } from "@/auth";
 import SearchBar from "./component/SearchBar";
 import { GridLayoutStyle, QueryLayoutStyle, SearchBarLayoutStyle } from "./style.css";
 import Query from "./component/Query";
+import { redirect } from "next/navigation";
+
+const shortenArea = [
+    "서울", "인천", "경기", "강원", "충남", "대전", "세종", "충북", "경북", "대구", "광주", "전남", "전북", "경남", "울산", "부산", "제주"
+]
+
 
 export default async function Layout({ params, children }: { params: { map: string[] },  children: React.ReactNode}) {
     const session = await auth()
@@ -10,6 +16,9 @@ export default async function Layout({ params, children }: { params: { map: stri
     
     // 리다이렉션시 index로 바뀜
     if(at_id == 'index') at_id = undefined
+
+    // 지역으로 리다이렉션시 홈으로
+    if(shortenArea.includes(decodeURI(at_id))) redirect("/")
 
     // 왜인지 모르겠는데 router.push로 돌아올때 at_id가 index로 나옴
     if(at_id && at_id !== 'index'){
