a4605e7650c8db4238fc6b3a41010c91734c5f45
ChoiYongWon <yongwon0824@naver.com>
2024-05-22 10:53:00 +0900

    feat: #23 GPS 최대 캐시 30초

--- app/_common/util/location.ts
+++ app/_common/util/location.ts
@@ -1,5 +1,7 @@
 export const getPosition = ():Promise<GeolocationPosition> => {
 	return new Promise((resolve, reject) =>
-    	navigator.geolocation.getCurrentPosition(resolve, reject)
+    	navigator.geolocation.getCurrentPosition(resolve, reject, {
+			maximumAge: 1000 * 30 // 캐시 30초
+		})
 	);
 };
