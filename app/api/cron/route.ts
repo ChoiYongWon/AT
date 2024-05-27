a23f621f9666433188e087fded1a6c622c00a471
ChoiYongWon <yongwon0824@naver.com>
2024-05-27 17:12:08 +0900

    feat: #23 Cron 캐시 제거

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,11 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+export const dynamic = 'force-dynamic'
+
+import { NextResponse } from "next/server";
 import { InternalServerError } from "../error/server/InternalServer.error";
 
-
-// prewarming용 route api
 export async function GET() {
-
   try {
+    
     return new NextResponse(
         JSON.stringify({ data: true, message: "Cron Successfully Prewarming." }),
         { status: 200, headers: { "content-type": "application/json" } }
