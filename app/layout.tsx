5ba2a67b3535ff4f2c2978780a5897ecf78af3b8
ChoiYongWon <yongwon0824@naver.com>
2024-06-18 23:14:16 +0900

    test: #23 root layout client component로 바꿈

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import type { Metadata, Viewport } from "next";
 // import { Inter } from "next/font/google";
 import "./global.css";
@@ -18,32 +16,32 @@ import { Poppins } from 'next/font/google'
 
 // const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "AT - A Spot Thur",
-//   description: "나만의 지도를 만들어보세요!",
-//   openGraph: {
-//     title: "AT - A Spot Thur",
-//     description: "나만의 지도를 만들어보세요!",
-//     images: "https://github.com/ChoiYongWon/AT/assets/33446738/4857e99c-0610-484d-a7a5-acc1311ef2a9",
-//     type: "website",
-//     url: "https://a-spot-thur.app/",
-//   },
-//   manifest: "/manifest.json",
-//   icons: [
-//     { rel: "icon", url: "/icons/icon-192x192.png", sizes: "192x192" }
-//     // 추가 아이콘 정보
-//   ],
-//   other: {
-//     google: 'notranslate'
-//   }
-// };
+export const metadata: Metadata = {
+  title: "AT - A Spot Thur",
+  description: "나만의 지도를 만들어보세요!",
+  openGraph: {
+    title: "AT - A Spot Thur",
+    description: "나만의 지도를 만들어보세요!",
+    images: "https://github.com/ChoiYongWon/AT/assets/33446738/4857e99c-0610-484d-a7a5-acc1311ef2a9",
+    type: "website",
+    url: "https://a-spot-thur.app/",
+  },
+  manifest: "/manifest.json",
+  icons: [
+    { rel: "icon", url: "/icons/icon-192x192.png", sizes: "192x192" }
+    // 추가 아이콘 정보
+  ],
+  other: {
+    google: 'notranslate'
+  }
+};
 
-// export const viewport:Viewport = {
-//   width: "device-width",
-//   initialScale: 1,
-//   userScalable: false,
-//   themeColor: "#f7f7f7",
-// }
+export const viewport:Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  userScalable: false,
+  themeColor: "#f7f7f7",
+}
 
 const nanumSquareNeo = localFont({
   src: './_asset/fonts/NanumSquareNeo-Variable.woff2',
